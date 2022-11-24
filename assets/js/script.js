53991945bd8d6d3eeb3d4d22bc1c61f183e0c2d3
sanchezju <sjuan634@yahoo.com>
2022-11-23 22:13:05 -0800

    rewrite js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,54 @@
-const mainEl = document.querySelector('main');
+var mainEl = document.querySelector('main');
 
-const moviegluSandbox = {
-  filmsNowShowingEndpoint: 'https://api-gate2.movieglu.com/filmsNowShowing/',
-  filmShowTimesEndpoint: 'https://api-gate2.movieglu.com/filmShowTimes/',
-  cinemaDetailsEndpoint: 'https://api-gate2.movieglu.com/cinemaDetails/',
+function successPosition (position) {
+  var userLocation = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude
+  }
+  console.log(userLocation)
+  function cinemasNearbyData() {
+    fetch(moviegluCall.cinemasNearbyEndpoint, {
+      "headers": {
+        "api-version": moviegluCall.apiVersion,
+        "Authorization": moviegluCall.authorization,
+        "client": moviegluCall.client,
+        "x-api-key": moviegluCall.apikey,
+        "device-datetime": moviegluCall.datetime,
+        "territory": moviegluCall.territory,
+        "geolocation": `${userLocation.latitude};${userLocation.longitude}`
+      },
+    })
+    .then(function (response) {
+      return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        return data;
+      });
+    }
+  cinemasNearbyData();
+}
+
+function errorPosition (error) {
+  console.log(error);
+}
+
+navigator.geolocation.getCurrentPosition(successPosition, errorPosition);
+
+const moviegluCall = {
+  cinemasNearbyEndpoint: 'https://api-gate2.movieglu.com/cinemasNearby/',
+  cinemaShowtimesEndpoint: 'https://api-gate2.movieglu.com/cinemaShowTimes/',
+  apiVersion: 'v200',
+  authorization: 'Basic UkhUTjpYQk5RaDBDNGhPamM=',
+  client: 'RHTN',
+  apikey: 'qnpVdOgChm42R0SlChf4OgIAJS6b5ZN2QHGKUFv4',
+  datetime: new Date(Date.now()).toISOString(),
+  territory: 'US'
+}
+
+var moviegluSandbox = {
+  cinemasNearbyEndpoint: 'https://api-gate2.movieglu.com/cinemasNearby/',
+  cinemaShowtimesEndpoint: 'https://api-gate2.movieglu.com/cinemaShowTimes/',
   apiVersion: 'v200',
   authorization: 'Basic UkhUTl9YWDpPWDVQeDNKS0NVY1Q=',
   client: 'RHTN',
@@ -12,106 +57,113 @@ const moviegluSandbox = {
   territory: 'XX'
 }
 
-async function fetchFilmsNowShowing() {
-  const response = await fetch(moviegluSandbox.filmsNowShowingEndpoint, {
-    "headers": {
-        "api-version": moviegluSandbox.apiVersion,
-        "Authorization": moviegluSandbox.authorization,
-        "client": moviegluSandbox.client,
-        "x-api-key": moviegluSandbox.apikey,
-        "device-datetime": moviegluSandbox.datetime,
-        "territory": moviegluSandbox.territory,
-    },
-});
-  const data = await response.json();
-  return data;
-}
 
-async function fetchFilmShowTimes(filmID, date) {
-  const response = await fetch(`${moviegluSandbox.filmShowTimesEndpoint}?film_id=${filmID}&date=${date}`, {
-    "headers": {
-        "api-version": moviegluSandbox.apiVersion,
-        "Authorization": moviegluSandbox.authorization,
-        "client": moviegluSandbox.client,
-        "x-api-key": moviegluSandbox.apikey,
-        "device-datetime": moviegluSandbox.datetime,
-        "territory": moviegluSandbox.territory,
-    },
-});
-  const data = await response.json();
-  return data;
-}
 
-async function fetchCinemaDetails(cinemaID) {
-  const response = await fetch(`${moviegluSandbox.filmsNowShowingEndpoint}?cinema_id=${cinemaID}`, {
-    "headers": {
-        "api-version": moviegluSandbox.apiVersion,
-        "Authorization": moviegluSandbox.authorization,
-        "client": moviegluSandbox.client,
-        "x-api-key": moviegluSandbox.apikey,
-        "device-datetime": moviegluSandbox.datetime,
-        "territory": moviegluSandbox.territory,
-    },
-});
-  const data = await response.json();
-  return data;
-}
 
-async function getFilmsNowShowing () {
-const filmsNowShowingData = await fetchFilmsNowShowing();
-return filmsNowShowingData;
-}
+// console.log(cinemasNearbyData)
 
-async function getFilmShowTimes () {
-const filmShowTimesData = await fetchFilmShowTimes();
-console.log(filmShowTimesData);
-}
+// async function fetchFilmsNowShowing() {
+//   const response = await fetch(moviegluCall.cinemasNearbyEndpoint, {
+//     "headers": {
+//         "api-version": moviegluCall.apiVersion,
+//         "Authorization": moviegluCall.authorization,
+//         "client": moviegluCall.client,
+//         "x-api-key": moviegluCall.apikey,
+//         "device-datetime": moviegluCall.datetime,
+//         "territory": moviegluCall.territory,
+//     },
+// });
+//   const data = await response.json();
+//   console.log(data)
+//   return data;
+// }
 
-async function getCinemaDetails () {
-const cinemaDetailsData = await fetchCinemaDetails();
-console.log(cinemaDetailsData);
-}
+// async function fetchFilmShowTimes(filmID, date) {
+//   const response = await fetch(`${moviegluCall.cinemaShowtimesEndpoint}?film_id=${filmID}&date=${date}`, {
+//     "headers": {
+//         "api-version": moviegluCall.apiVersion,
+//         "Authorization": moviegluCall.authorization,
+//         "client": moviegluCall.client,
+//         "x-api-key": moviegluCall.apikey,
+//         "device-datetime": moviegluCall.datetime,
+//         "territory": moviegluCall.territory,
+//     },
+// });
+//   const data = await response.json();
+//   return data;
+// }
 
-async function buildNowShowingList () {
-  const filmsNowShowingData = await getFilmsNowShowing();
-  for (const films of filmsNowShowingData.films) {
-    const nowShowingListDiv = document.createElement('div');
-    const nowShowingFilmDiv = document.createElement('div');
-    nowShowingFilmDiv.setAttribute('id', films.film_id);
-    const movieTitleEl = document.createElement('h2');
-    movieTitleEl.textContent = films.film_name;
+// async function fetchCinemaDetails(cinemaID) {
+//   const response = await fetch(`${moviegluCall.cinemasNearbyEndpoint}?cinema_id=${cinemaID}`, {
+//     "headers": {
+//         "api-version": moviegluCall.apiVersion,
+//         "Authorization": moviegluCall.authorization,
+//         "client": moviegluCall.client,
+//         "x-api-key": moviegluCall.apikey,
+//         "device-datetime": moviegluCall.datetime,
+//         "territory": moviegluCall.territory,
+//     },
+// });
+//   const data = await response.json();
+//   return data;
+// }
 
-    if (films.images.poster.length === undefined) {
-      var moviePosterEl = document.createElement('img');
-      moviePosterEl.setAttribute('src', films.images.poster[1].medium.film_image);
-      moviePosterEl.setAttribute('alt', 'Movie Poster');
-    } else {
-      var moviePosterEl = document.createElement('img');
-      moviePosterEl.setAttribute('src', './assets/images/placeholder.png');
-      moviePosterEl.setAttribute('alt', 'Movie poster not available');
-    }
+// async function getFilmsNowShowing () {
+// const filmsNowShowingData = await fetchFilmsNowShowing();
+// return filmsNowShowingData;
+// }
 
-    const ageRatingEl = document.createElement('span');
-    ageRatingEl.textContent = films.age_rating[0].rating;
-    const releaseDateEl = document.createElement('span');
-    releaseDateEl.textContent = films.release_dates[0].release_date;
-    const synopsisEl = document.createElement('p');
-    synopsisEl.textContent = films.synopsis_long;
-    const showTimeBtnEl = document.createElement('button');
-    showTimeBtnEl.textContent = 'View Showtimes';
+// async function getFilmShowTimes () {
+// const filmShowTimesData = await fetchFilmShowTimes();
+// console.log(filmShowTimesData);
+// }
 
-    nowShowingFilmDiv.appendChild(movieTitleEl);
-    nowShowingFilmDiv.appendChild(moviePosterEl);
-    nowShowingFilmDiv.appendChild(releaseDateEl);
-    nowShowingFilmDiv.appendChild(ageRatingEl);
-    nowShowingFilmDiv.appendChild(showTimeBtnEl);
-    nowShowingFilmDiv.appendChild(synopsisEl);
-    nowShowingListDiv.appendChild(nowShowingFilmDiv);
-    mainEl.appendChild(nowShowingListDiv);
-  }
-  return mainEl;
-}
+// async function getCinemaDetails () {
+// const cinemaDetailsData = await fetchCinemaDetails();
+// console.log(cinemaDetailsData);
+// }
 
+// async function buildNowShowingList () {
+//   const filmsNowShowingData = await getFilmsNowShowing();
+//   for (const films of filmsNowShowingData.films) {
+//     const nowShowingListDiv = document.createElement('div');
+//     const nowShowingFilmDiv = document.createElement('div');
+//     nowShowingFilmDiv.setAttribute('id', films.film_id);
+//     const movieTitleEl = document.createElement('h2');
+//     movieTitleEl.textContent = films.film_name;
+
+//     if (films.images.poster.length === undefined) {
+//       var moviePosterEl = document.createElement('img');
+//       moviePosterEl.setAttribute('src', films.images.poster[1].medium.film_image);
+//       moviePosterEl.setAttribute('alt', 'Movie Poster');
+//     } else {
+//       var moviePosterEl = document.createElement('img');
+//       moviePosterEl.setAttribute('src', './assets/images/placeholder.png');
+//       moviePosterEl.setAttribute('alt', 'Movie poster not available');
+//     }
+
+//     const ageRatingEl = document.createElement('span');
+//     ageRatingEl.textContent = films.age_rating[0].rating;
+//     const releaseDateEl = document.createElement('span');
+//     releaseDateEl.textContent = films.release_dates[0].release_date;
+//     const synopsisEl = document.createElement('p');
+//     synopsisEl.textContent = films.synopsis_long;
+//     const showTimeBtnEl = document.createElement('button');
+//     showTimeBtnEl.textContent = 'View Showtimes';
+
+//     nowShowingFilmDiv.appendChild(movieTitleEl);
+//     nowShowingFilmDiv.appendChild(moviePosterEl);
+//     nowShowingFilmDiv.appendChild(releaseDateEl);
+//     nowShowingFilmDiv.appendChild(ageRatingEl);
+//     nowShowingFilmDiv.appendChild(showTimeBtnEl);
+//     nowShowingFilmDiv.appendChild(synopsisEl);
+//     nowShowingListDiv.appendChild(nowShowingFilmDiv);
+//     mainEl.appendChild(nowShowingListDiv);
+//   }
+//   return mainEl;
+// }
+
+// buildNowShowingList()
 
 // const showtimeBtns = document.querySelectorAll('button');
 // console.log(showtimeBtns);
@@ -126,17 +178,91 @@ async function buildNowShowingList () {
 
 
 
-// function successPosition (position) {
-// //   console.log(position);
-//   const userLocation = {
-//     latitude: position.coords.latitude,
-//     longitude: position.coords.longitude
+
+//key
+//Hide google key later
+// const API_KEY = 'T05NSjpBNmlxMFdTc2lENDc=';
+// const CLIENT_NAME = 'ONMJ';
+// const X_API_KEY = 'rBsn3QcluW9RVPyYaBUE43nwuB0TzQgs3hA9AsbT';
+// const API_VERSION = 'v200';
+// const TERRITORY = 'US';
+// ​
+// const latInp = document.querySelector('.lat-inp');
+// const longInp = document.querySelector('.long-inp');
+// const addressForm = document.querySelector('.address-form');
+// const resMsgWrapper = document.querySelector('.res-msg-wrapper');
+// const theatersMapDiv = document.querySelector('.theaters-map');
+// //theater lan-long I have to change that later but not cleat how I can do that
+// // sometimes its shit doesnt work fix that later
+// function getNearbyTheaters() {
+//   return new Promise(function(resolve, reject){
+//     fetch('https://api-gate2.movieglu.com/cinemasNearby/?n=10', {
+//       method: 'GET',
+//       headers: {
+//         "api-version": API_VERSION,
+//         "Authorization": `Basic ${API_KEY}`,
+//         "client": CLIENT_NAME,
+//         "x-api-key": X_API_KEY,
+//         "device-datetime": new Date().toISOString(),
+//         "territory": TERRITORY,
+//         "geolocation": `${latInp.value};${longInp.value}`
+//       }
+//     })
+//     .then(function(res){
+//       if(res.ok) return res.json();
+//       reject('No theaters found nearby');
+//     })
+//     .then(function(data){
+//       if(!data.cinemas.length) reject('No theaters found nearby');
+//       resolve(data);
+//     })
+//     .catch(function(err){ 
+//       reject('Something went wrong');
+//     })
+//   });  
+// }
+// // change for user location 
+// async function showNearbyTheaters(e) {
+//   e.preventDefault();
+// ​
+//   try {
+//     const data = await getNearbyTheaters();
+//     resMsgWrapper.className = 'res-msg-wrapper success';
+//     resMsgWrapper.innerText = `${data.cinemas.length} theaters found nearby`;
+//     initMap(data.cinemas);
+//   } catch (err) {
+//     resMsgWrapper.className = 'res-msg-wrapper error';
+//     resMsgWrapper.innerText = err;
+//     theatersMapDiv.innerHTML = '';
 //   }
-//   return userLocation;
 // }
-
-// function errorPosition (error) {
-//   console.log(error);
+// //map check zoom one more time 
+// //make zoom more that 3
+// function initMap(theaters) { const map = new google.maps.Map(theatersMapDiv, {
+//     zoom: 3,
+//     center: { lat: Number(latInp.value), lng: Number(longInp.value) },
+//   });
+// ​
+//   const infoWindow = new google.maps.InfoWindow({
+//     content: '',
+//     disableAutoPan: true,
+//   });
+// ​
+//   const markers = theaters.map((theater, i) => {
+//     const marker = new google.maps.Marker({
+//       position: { lat: theater.lat, lng: theater.lng },
+//       label: '',
+//     });
+// ​
+//     marker.addListener('click', function() {
+//       infoWindow.setContent(theater.cinema_name + '<br/>' + theater.address);
+//       infoWindow.open(map, marker);
+//     });
+//     return marker;
+//   });
+// ​
+//   new markerClusterer.MarkerClusterer({ map, markers });
 // }
-
-// navigator.geolocation.getCurrentPosition(successPosition, errorPosition);
+// ​
+// ​
+// addressForm.addEventListener('submit', showNearbyTheaters);
